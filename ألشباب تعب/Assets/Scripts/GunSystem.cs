838b6b29db28f68232aeb9c6dd22b29a2cea5fa7
bad.exe <68984330+bad-boy-yyy@users.noreply.github.com>
2021-08-28 00:33:16 +0200

    i wanna die

---
+++ ألشباب تعب/Assets/Scripts/GunSystem.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GunSystem : MonoBehaviour
+{
+    [Header("Gun Variables")]
+    public string GunName;
+    public Image GunImage;
+    public Transform gunBarrel;
+    public float firerate, spread, recoil, ammo, maxAmmo, reloadTime;
+    public GameObject bulletProjectile;
+    public bool isAutomatic;
+
+
+
+    // Graphics
+    public ParticleSystem muzzleFlash;
+
+    // Private refrences
+    bool ableToShoot;
+
+
+
+    public void Start()
+    {
+        ableToShoot = true;
+    }
+
+    public void Update()
+    {
+        MyInput();
+    }
+
+    public void MyInput()
+    {
+        if (isAutomatic)
+        {
+            if (Input.GetMouseButton(0))
+            {
+                Shoot();
+            }
+        }
+        else
+        {
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                Shoot();
+            }
+
+        }
+    }
+    public void Shoot()
+    {
+        if (ableToShoot)
+        {
+            ableToShoot = false;
+            // GameObject bullet = Instantiate(bulletProjectile, gunBarrel.position, Quaternion.identity);
+            Debug.Log("Pew Pew!");
+
+
+
+
+            Invoke("ResetShot", firerate);
+        }
+    }
+    public void ResetShot()
+    {
+        ableToShoot = true;
+    }
+
+
+
+}
