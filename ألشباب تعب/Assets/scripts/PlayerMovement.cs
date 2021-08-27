2c558b55fc6dcefd7903631d923d61a8f0b088ba
ElGhostGames <79412233+ElGhostGames@users.noreply.github.com>
2021-08-27 21:36:21 +0200

    Player Movement

---
+++ ألشباب تعب/Assets/scripts/PlayerMovement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float speed;
+
+    void Update()
+    {
+        float hor = Input.GetAxis("Horizontal");
+        float ver = Input.GetAxis("Vertical");
+
+        transform.position += new Vector3(hor * speed * Time.deltaTime, 0, ver * speed * Time.deltaTime);
+    }
+}
