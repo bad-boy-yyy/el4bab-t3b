0523ec8402931ebba9063d39adea1a99f8c86787
ElGhostGames <79412233+ElGhostGames@users.noreply.github.com>
2021-08-27 23:16:57 +0200

    Movement

---
+++ ألشباب تعب/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float pcSpeed;
+    [HideInInspector]
+    public float MBSpeed;
+
+    public LayerMask canJumpLayer;
+
+    private bool moveRight;
+    private bool moveLeft;
+
+    public float jumpForce;
+    [HideInInspector]
+    public bool infiniteJump;
+    [HideInInspector]
+    public int jumpCount = 2;
+
+    private Rigidbody2D rb;
+
+    private Vector2 myPos;
+    private Camera cam;
+    public float cameraSmoothness;
+
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+        cam = Camera.main;
+    }
+
+    void FixedUpdate()
+    {
+        PCController();
+        CameraFollow();
+    }
+
+    private void PCController()
+    {
+        float hor = Input.GetAxis("Horizontal");
+        float ver = Input.GetAxis("Vertical");
+
+        Vector3 move = new Vector2(hor * Time.deltaTime * pcSpeed, ver * Time.deltaTime * pcSpeed);
+
+        rb.velocity = move;
+
+        //transform.position += move;
+    }
+
+    private void CameraFollow()
+    {
+        myPos = new Vector3(transform.position.x, transform.position.y);
+        cam.transform.position = Vector3.Lerp(cam.transform.position, myPos, cameraSmoothness * Time.deltaTime);
+        cam.transform.position = new Vector3(cam.transform.position.x, cam.transform.position.y, -10);
+    }
+}
