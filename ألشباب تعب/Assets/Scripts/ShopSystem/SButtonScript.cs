7636876ba3e37e1e21a7bdafbbcaa600b5ad69c9
ElGhostGames <79412233+ElGhostGames@users.noreply.github.com>
2021-08-28 17:56:07 +0200

    Shop System
    Very Very cool shop and money system

---
+++ ألشباب تعب/Assets/Scripts/ShopSystem/SButtonScript.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class SButtonScript : MonoBehaviour
+{
+    public int price;
+
+    [HideInInspector]public bool checkedBool;
+
+    [HideInInspector]public Button thisButton;
+
+    private ShopScript shopScript;
+
+    public int index;
+
+    private void Start()
+    {
+        thisButton = this.GetComponent<Button>();
+
+        shopScript = GameObject.Find("Shop").GetComponent<ShopScript>();
+    }
+
+    private void Update()
+    {
+        int moneyDiff = shopScript.Money - shopScript.cost;
+
+        if (checkedBool)
+        {
+            GetComponent<Image>().color = Color.gray;
+        }
+        else
+        {
+            GetComponent<Image>().color = Color.green;
+        }
+
+        if (moneyDiff >= price)
+        {
+            thisButton.enabled = true;
+        }
+        else
+        {
+            if (!checkedBool)
+            {
+                thisButton.enabled = false;
+                GetComponent<Image>().color = Color.red;
+            }
+        }
+    }
+
+    public void BoolFunction()
+    {
+        if (!checkedBool)
+        {
+            checkedBool = true;
+            shopScript.cost += price;
+        }
+        else
+        {
+            checkedBool = false;
+            shopScript.cost -= price;
+        }
+    }
+
+    public void BuyFunction()
+    {
+        //Call the buying Function...
+
+        if (checkedBool)
+        shopScript.MainVoid(index);
+    }
+
+}
--- ألشباب تعب/Assets/Scripts/ShopSystem/ShopScript.cs
+++ ألشباب تعب/Assets/Scripts/ShopSystem/ShopScript.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ShopScript : MonoBehaviour
 {
@@ -13,6 +14,17 @@ public class ShopScript : MonoBehaviour
     public GameObject shopPanel;
 
     public float shopDistance;
+
+    public int Money;
+    public int cost;
+    private int remaining;
+
+    public Text moneyTxt;
+    public Text costTxt;
+    public Text remainingTxt;
+
+    public GameObject[] UpgradeButtons;
+
     void Start()
     {
         player = GameObject.Find("Player");
@@ -20,6 +32,12 @@ public class ShopScript : MonoBehaviour
 
     void Update()
     {
+        remaining = Money - cost;
+
+        moneyTxt.text = "Money : " + Money.ToString();
+        costTxt.text = "Cost : " + cost.ToString();
+        remainingTxt.text = "Remaining : " + remaining.ToString();
+
         CanOpenFunction();
     }
 
@@ -55,4 +73,124 @@ public class ShopScript : MonoBehaviour
             shopPanel.SetActive(false);
         }
     }
+
+    public void BuyButton()
+    {
+        //decrease the money by....
+        Money -= cost;
+
+        //reset the cost...
+        cost = 0;
+
+        for (int i = 0; i < UpgradeButtons.Length; i++)
+        {
+            UpgradeButtons[i].GetComponent<SButtonScript>().BuyFunction();
+
+            UpgradeButtons[i].GetComponent<SButtonScript>().checkedBool = false;
+        }
+    }
+
+    public void ResetButton()
+    {
+        //reset the upgrades that i have chosen...
+
+        cost = 0;
+
+        for (int i = 0; i < UpgradeButtons.Length; i++)
+        {
+            UpgradeButtons[i].GetComponent<SButtonScript>().checkedBool = false;
+        }
+    }
+
+    public void BackButton()
+    {
+        shopPanel.SetActive(false);
+    }
+
+    #region UpgradesFunctions
+
+    public void MainVoid(int index)
+    {
+        //apply the function that we have chosen by the button index...
+
+        int rand = index;
+
+        switch (index)
+        {
+            case 0:
+                Drink();
+                break;
+
+            case 1:
+                Eat();
+                break;
+
+            case 2:
+                IncreaseSpeed();
+                break;
+
+            case 3:
+                IncreaseHealth();
+                break;
+
+            case 4:
+                DamageIncrease();
+                break;
+
+            case 5:
+                Bullet10();
+                break;
+
+            case 6:
+                Bullet20();
+                break;
+
+            case 7:
+                Bullet50();
+                break;
+        }
+    }
+
+    //we will put the commands in the below functions..
+
+    private void Drink()
+    {
+        Debug.Log("Drink");
+    }
+
+    private void Eat()
+    {
+        Debug.Log("Eat");
+    }
+
+    private void IncreaseSpeed()
+    {
+        Debug.Log("IncreaseSpeed");
+    }
+
+    private void IncreaseHealth()
+    {
+        Debug.Log("IncreaseHealth");
+    }
+
+    private void DamageIncrease()
+    {
+        Debug.Log("DamageIncrease");
+    }
+
+    private void Bullet10()
+    {
+        Debug.Log("Bullet10");
+    }
+
+    private void Bullet20()
+    {
+        Debug.Log("Bullet20");
+    }
+
+    private void Bullet50()
+    {
+        Debug.Log("Bullet50");
+    }
+    #endregion
 }
