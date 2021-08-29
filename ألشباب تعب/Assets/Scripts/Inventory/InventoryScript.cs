981188367d8d0831e5c1754ebe756866d43eb96e
ElGhostGames <79412233+ElGhostGames@users.noreply.github.com>
2021-08-29 19:05:06 +0200

    Nothing

---
+++ ألشباب تعب/Assets/Scripts/Inventory/InventoryScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryScript : MonoBehaviour
+{
+
+    public GameObject InventoryPanel;
+
+    public void InventoryButton()
+    {
+        InventoryPanel.SetActive(true);
+    }
+
+}
